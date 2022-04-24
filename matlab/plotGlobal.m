ab0a3983c900df6a12d08effee477722a12ca2a4
kenhasteandersen <kha@aqua.dtu.dk>
2022-04-24 11:18:24 +0200

    Made setup before plotting

--- matlab/plotGlobal.m
+++ matlab/plotGlobal.m
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 addpath('~/NUMmodel/matlab')
 
-p = setupGeneralistsOnly(20,true);
-p = parametersGlobal(p,2);
-=======
+p = setupGeneralistsOnly(20);
 %p = setupGeneralistsOnly(20,true);
-%p = parametersGlobal(p,2);
->>>>>>> b4a02ff95bf283b443b333257724866e92e9bdcc
+p = parametersGlobal(p,2);
 
 %sim = simulateGlobal(p);
 
